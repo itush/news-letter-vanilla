434c1a75e1cb9ec8dc625bc9852444c3882baa86
Tushar <itusharbiswas@gmail.com>
2023-07-04 15:43:44 +0530

    draft 1.0.2

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-/*Selecting the form element*/
+/*Selecting the form element, email input, error message, 
+sign-up, success message, user email & dismiss button */
 const form = document.querySelector('.form');
-
-/*Selecting the email input*/
 const emailInput = document.getElementById('email');
-
-/*Selecting the error message*/
 const errorMsg = document.querySelector('.error-msg');
-
-/*Selecting the sign-up, success message & user email*/
 const signUp = document.querySelector('.sign-up');
 const successMsg = document.querySelector('.success-message');
 const userEmail = document.querySelector('.user-email');
 const dismissBtn = document.querySelector('.dismiss-btn');
+
+/*Regex pattern for validating email*/
+const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/;
 
 /*Add an event listener to the form for the 'submit' event*/
 form.addEventListener('submit', (event) => {
@@ -19,30 +17,20 @@ form.addEventListener('submit', (event) => {
     /*Preventing the default form submission behavior */
     event.preventDefault();
 
-    
-
     /*Getting the value of the email input field and trimming it*/
     const email = emailInput.value.trim();
-   
 
     /*If email is empty*/
     if (email === '') {
         emailInput.style.border = '1px solid red';
         errorMsg.innerHTML = 'Please enter your email';
     }
-
-    /*If @ is absent*/
-    else if (email.indexOf('@') === -1) {
+    /*Validating the email pattern*/
+    else if (!emailPattern.test(email)) {
         emailInput.style.border = '1px solid red';
         errorMsg.innerHTML = 'Invalid email';
     }
-
-    /*If . is absent*/
-    else if (email.indexOf('.') === -1) {
-        emailInput.style.border = '1px solid red';
-        errorMsg.innerHTML = 'Invalid email';
-    }
-
+    /*If email is valid*/
     else {
         signUp.style.display = 'none';
         successMsg.style.display = 'block';
@@ -50,14 +38,12 @@ form.addEventListener('submit', (event) => {
         errorMsg.innerHTML = '';
         userEmail.innerHTML = email;
     }
+});
 
-}
-);
-
-
+/*Adding an event listener to the dismiss button*/
 dismissBtn.addEventListener('click', () => {
     successMsg.style.display = 'none';
     signUp.style.display = 'block';
     emailInput.value = '';
-    
+
 })
